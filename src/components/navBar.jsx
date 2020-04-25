f873e158b39f32295513c57737e80abcbce5ed97
AG <angappan.ganesan@freshworks.com>
2020-04-25 22:28:29 +0530

    Converting NavBar component to stateless function

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand">Total Item in Cart: {this.props.onGetCount()}</a>
-                </nav>
-            </div>
-         );
-    }
+const NavBar = (props) => {
+    const {onGetCount} = props
+    return ( 
+        <div>
+            <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand">Total Item in Cart: {onGetCount()}</a>
+            </nav>
+        </div>
+     );
 }
  
 export default NavBar;
